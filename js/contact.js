c24388975a26d05ebdf43f30e3dd2cbd94051b19
tomiproven <gaastonromano@gmail.com>
2022-01-23 14:35:25 -0300

    cambios en contacto

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'))
+    $buttonMailto.setAttribute('href', `mailto:gaastonromano@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+}
